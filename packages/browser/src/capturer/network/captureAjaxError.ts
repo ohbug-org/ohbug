4af4ca3fb8066197b20ae35479b18ba91cdc551b
chenyueban <chenyueban@qq.com>
2019-12-30 16:17:11 +0800

    feat: added breadcrumb support for ajax

--- packages/browser/src/capturer/network/captureAjaxError.ts
+++ packages/browser/src/capturer/network/captureAjaxError.ts
@@ -1,5 +1,5 @@
 import { getGlobal, warning, replace } from '@ohbug/utils'
-import { types } from '@ohbug/core'
+import { types, getHub } from '@ohbug/core'
 import { networkDispatcher } from '../../dispatcher'
 
 const global = getGlobal<Window>()
@@ -9,6 +9,8 @@ const { AJAX_ERROR } = types
  * capture AJAX_ERROR
  */
 function captureAjaxError() {
+  const hub = getHub<Window>()
+
   warning(
     'XMLHttpRequest' in global,
     'Ohbug: Binding `AJAX` monitoring failed, the current environment did not find the object `XMLHttpRequest`'
@@ -40,19 +42,27 @@ function captureAjaxError() {
       function(...args: any[]) {
         this.addEventListener('readystatechange', function() {
           if (this.readyState === 4) {
+            const data = {
+              req: {
+                url: desc.url,
+                method: desc.method,
+                data: args[0] || {}
+              },
+              res: {
+                status: this.status,
+                statusText: this.statusText,
+                response: this.response
+              }
+            }
+            const timestamp = new Date().getTime()
+            hub.addBreadcrumb({
+              type: 'ajax',
+              timestamp,
+              data
+            })
+
             if (!this.status || this.status >= 400) {
-              networkDispatcher(AJAX_ERROR, {
-                req: {
-                  url: desc.url,
-                  method: desc.method,
-                  data: args[0] || {}
-                },
-                res: {
-                  status: this.status,
-                  statusText: this.statusText,
-                  response: this.response
-                }
-              })
+              networkDispatcher(AJAX_ERROR, data)
             }
           }
         })
