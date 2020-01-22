255226f741abdc0d83cbf458595165b2185155ad
chenyueban <chenyueban@qq.com>
2020-01-22 21:53:09 +0800

    feat(plugin): added plugin-nemetric

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,11 +1,12 @@
 export { default as init } from './init'
 export { default as applyPlugin } from './applyPlugin'
-export { default as capturer, PluginCapturerContext } from './capturer'
+export { default as capturer } from './capturer'
 export { default as collector } from './collector'
 export { default as report } from './report'
 export { default as createEvent } from './createEvent'
 export { default as captureMessage } from './captureMessage'
 
+export * from './capturer'
 export * from './config'
 export * from './enhancer'
 export * from './hub'
