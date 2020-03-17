f6804aef448c9f500c87659a427dd24adf398fa1
chenyueban <chenyueban@qq.com>
2020-03-17 21:20:38 +0800

    test(core): test collector

---
+++ packages/core/__tests__/collector.test.ts
@@ -0,0 +1,76 @@
+import { getGlobal } from '@ohbug/utils'
+import applyPlugin from '../src/applyPlugin'
+import init from '../src/init'
+import collector from '../src/collector'
+import { getEnhancer } from '../src/enhancer'
+import { getHub } from '../src/hub'
+jest.mock('../src/hub')
+
+const apiKey = 'test_id'
+const config = { apiKey }
+const platform = 'browser'
+const event = { type: 'test' }
+const extra = 'hello'
+const plugin = () => ({
+  collector() {
+    return {
+      extra
+    }
+  }
+})
+const enhancer = applyPlugin(plugin)
+
+describe('core capturer', () => {
+  afterEach(() => {
+    // destroy
+    const global = getGlobal()
+    delete global.__OHBUG__
+  })
+
+  it('calls hub.addEvent with enhancer', () => {
+    init({
+      config,
+      platform,
+      version: 'test',
+      handleCapture: () => {},
+      handleReport: () => {},
+      handleAsync: () => {},
+      enhancer
+    })
+
+    const mockAddEvent = jest.fn()
+    ;(getHub as jest.Mock).mockImplementation(() => ({
+      addEvent: mockAddEvent
+    }))
+
+    collector(event)
+
+    const _enhancer = getEnhancer()
+    if (_enhancer && Array.isArray(_enhancer.collectors) && _enhancer.collectors.length) {
+      expect(mockAddEvent.mock.calls[0][0]).toEqual({ ...event, state: { extra } })
+    }
+  })
+
+  it('calls hub.addEvent without enhancer', () => {
+    init({
+      config,
+      platform,
+      version: 'test',
+      handleCapture: () => {},
+      handleReport: () => {},
+      handleAsync: () => {}
+    })
+
+    const mockAddEvent = jest.fn()
+    ;(getHub as jest.Mock).mockImplementation(() => ({
+      addEvent: mockAddEvent
+    }))
+
+    collector(event)
+
+    const _enhancer = getEnhancer()
+    if (!(_enhancer && Array.isArray(_enhancer.collectors) && _enhancer.collectors.length)) {
+      expect(mockAddEvent.mock.calls[0][0]).toEqual(event)
+    }
+  })
+})
