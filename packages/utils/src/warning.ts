2bdbdd6ec9c4092cb25d536352b39e927ce971e0
chenyueban <chenyueban@qq.com>
2020-03-18 13:45:31 +0800

    fix(utils): warning use logger.error instead of throw error

--- packages/utils/src/warning.ts
+++ packages/utils/src/warning.ts
@@ -1,14 +1,17 @@
+import { logger } from './logger'
 const { NODE_ENV } = process.env
 
 export function warning(condition: boolean, format: string, ...args: any[]) {
   if (NODE_ENV !== 'production') {
     if (format === undefined) {
-      throw new Error('`warning(condition, format, ...args)` requires a warning message argument')
+      logger.error('`warning(condition, format, ...args)` requires a warning message argument')
+      return
     }
   }
 
   if (!condition) {
     let argIndex = 0
-    throw new Error(format.replace(/%s/g, () => args[argIndex++]))
+    logger.error(format.replace(/%s/g, () => args[argIndex++]))
+    return
   }
 }
