16104e5637512e4e12708fefebdf7e32bf1445a2
chenyueban <jasonchan0527@gmail.com>
2020-07-13 10:44:03 +0800

    test(browser): repair notify test file

--- packages/ohbug-browser/__tests__/notifier.test.ts
+++ packages/ohbug-browser/__tests__/notifier.test.ts
@@ -1,11 +1,14 @@
-import type { OhbugEvent } from '@ohbug/types'
+import { BrowserClient } from '../src/client'
+import type { OhbugEventWithMethods } from '@ohbug/types'
 import { notifier } from '../src/notifier'
 
 const mockSendBeacon = jest.fn()
-const event = { type: 'test' } as OhbugEvent<any>
+const event = { type: 'test' } as OhbugEventWithMethods<any>
+const apiKey = 'API_KEY_TEST'
 
 describe('@ohbug/browser/client', () => {
   it('notify via `navigator.sendBeacon`', () => {
+    BrowserClient.init({ apiKey })
     navigator.sendBeacon = mockSendBeacon
 
     notifier(event)
