d6c5f81fdc5f439441689ef83a1d73ab7ea4dc0f
chenyueban <jasonchan0527@gmail.com>
2020-09-02 14:06:47 +0800

    chore(package.json): modify more package configurations to adapt to more platforms

--- packages/ohbug-extension-rrweb/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/ohbug-extension-rrweb.cjs.prod.js')
-} else {
-  module.exports = require('./dist/ohbug-extension-rrweb.cjs.js')
-}
