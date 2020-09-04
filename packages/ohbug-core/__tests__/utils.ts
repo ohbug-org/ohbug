f8155397de48289b70d5b8377a0573d0c91cdb33
chenyueban <jasonchan0527@gmail.com>
2020-09-04 13:53:54 +0800

    test(core): adapt the structure of the new sdk, fix test errors

--- packages/ohbug-core/__tests__/utils.ts
+++ packages/ohbug-core/__tests__/utils.ts
@@ -1,15 +1,17 @@
-import { OhbugConfig, OhbugEventWithMethods } from '@ohbug/types'
+import { OhbugConfig, OhbugEventWithMethods, OhbugGetDevice } from '@ohbug/types'
 
 export const apiKey = 'API_KEY_TEST'
-export const device = {
+export const sdk = {
   platform: 'jest',
   version: '0.0.0',
 }
+export const getDevice: OhbugGetDevice = () => ({})
 export const getValues = (config?: OhbugConfig) => {
   config = config || { apiKey }
   return {
+    sdk,
     config,
-    device,
+    device: getDevice,
     notifier: (event: OhbugEventWithMethods<any>) => event,
   }
 }
