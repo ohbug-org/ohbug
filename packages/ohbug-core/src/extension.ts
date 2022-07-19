088c494581d17549770931f819f18d2c376b4b30
chenyueban <jasonchan0527@gmail.com>
2022-07-19 17:57:32 +0800

    fix(core): fix defineExtension return type error

--- packages/ohbug-core/src/extension.ts
+++ packages/ohbug-core/src/extension.ts
@@ -1,6 +1,6 @@
 import type { OhbugExtension } from '@ohbug/types'
 
 export function defineExtension(extension: OhbugExtension) {
-  if (!extension) return {}
+  if (!extension) return {} as OhbugExtension
   return extension
 }
